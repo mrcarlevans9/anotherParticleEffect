5e1632360598a2cc383a2cba7d2d4f963799157f
mrcarlevans9 <mrcarlevans9@gmail.com>
2021-03-13 16:15:10 -0500

    added animations to circle element

--- script.js
+++ script.js
@@ -13,20 +13,39 @@ canvas.height = window.innerHeight;
 });
 
 const mouse={
-    x:null,
-    y:null,
+    x:undefined,
+    y:undefined,
+    //by setting x and y to undefined the canvas is black until it is interacted with 
+    
 }
 canvas.addEventListener('click',function(event){
     mouse.x = event.x;
-    
+    mouse.y = event.y;
+   
+
+});
+
+canvas.addEventListener('mousemove',function(event){
+    mouse.x = event.x;
+    mouse.y = event.y;
+
 })
 
+function drawCircle(){
 
-ctx.fillStyle ='red';
-ctx.strokeStyle ='red';
-ctx.lineWidth =5;
-ctx.beginPath();
-ctx.arc(100,100,50,0,Math.PI*2);
-ctx.stroke();
+    ctx.fillStyle ='green';
+    ctx.beginPath();
+    ctx.arc( mouse.x,mouse.y,50,0,Math.PI*2);
+    ctx.fill();
 
 
+}
+function animate(){
+    ctx.clearRect(0,0,canvas.width,canvas.height);
+    drawCircle();
+    requestAnimationFrame(animate);
+    //by setting argument for requestAnimationFrame to animate this will create a loop
+
+
+}
+animate();
