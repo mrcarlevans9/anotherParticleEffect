63a0f7659b1799701e013e02114c9bc5eed7affa
mrcarlevans9 <mrcarlevans9@gmail.com>
2021-03-13 19:13:09 -0500

    added constilation effect by using pythagorium therom to connect particles

--- script.js
+++ script.js
@@ -35,9 +35,9 @@ canvas.addEventListener('mousemove',function(event){
     for(let i=0;i<4;i++){
         particlesArray.push(new Particle());
     }
-   // particlesArray.push(new Particle());
 
-})
+
+});
 
 class Particle {
     constructor(){
@@ -48,6 +48,7 @@ class Particle {
         this.size = Math.random()*15+1;//this assigns a particle size at random between 1 and 6
         this.speedx=Math.random()*3-1.5;
         this.speedy=Math.random()*3-1.5;
+        this.color ='hsl('+hue+',100%,50%)';
     }
     update(){
         this.x+=this.speedx;
@@ -55,17 +56,33 @@ class Particle {
         if(this.size>0.2) this.size-=0.1;
     }
     draw(){
-        ctx.fillStyle ='hsl('+hue+',100%,50%)';
+        ctx.fillStyle =this.color;
         ctx.beginPath();
         ctx.arc( this.x,this.y,this.size,0,Math.PI*2);
         ctx.fill();
     }
+    
 }
 
 function handleParticles(){
     for(let i = 0;i<particlesArray.length;i++){
         particlesArray[i].update();
         particlesArray[i].draw();
+        
+        for(let j=i; j < particlesArray.length; j++){
+            const dx = particlesArray[i].x-particlesArray[j].x;
+            const dy = particlesArray[i].y-particlesArray[j].y;
+            const distance = Math.sqrt(dx*dx+dy*dy);
+            if(distance <100){
+                ctx.beginPath();
+                ctx.strokeStyle = particlesArray[i].color;
+                ctx.linewidth = 0.2;
+                ctx.moveTo(particlesArray[i].x, particlesArray[i].y);
+                ctx.lineTo(particlesArray[j].x, particlesArray[j].y);
+                ctx.stroke();
+                ctx.closePath();
+            }
+        }
         if(particlesArray[i].size<=0.3){
             particlesArray.splice(i,1);
             i--;//this is used to keep from having blinking particles
@@ -74,11 +91,11 @@ function handleParticles(){
 }
 
 function animate(){
-    //ctx.clearRect(0,0,canvas.width,canvas.height);//this doesnt fade away slowly
-    ctx.fillStyle='rgb(0,0,0,0.5)';
-    ctx.fillRect(0,0,canvas.width,canvas.height);
+    ctx.clearRect(0,0,canvas.width,canvas.height);//this doesnt fade away slowly
+    //ctx.fillStyle='rgb(0,0,0,0.02)';
+    //ctx.fillRect(0,0,canvas.width,canvas.height);
     handleParticles();
-    hue++;
+    hue+=2;
     requestAnimationFrame(animate);
     //by setting argument for requestAnimationFrame to animate this will create a loop
 
